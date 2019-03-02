2c717a241f73cfabcb56ff0db236e2e4c8c80ed4
pseudokoder <pseudokoder@gmail.com>
2019-03-01 22:49:16 -0800

    updated

--- random_number_generator/random_number_generator.cpp
+++ random_number_generator/random_number_generator.cpp
@@ -4,6 +4,9 @@
 // Creates a file containing a list of size "n" random numbers in the specified
 // range.  File is labelled random_numbers.txt and is delimited by '/n'
 //
+// Citation: Many thanks to www.cplusplus.com for examples and references
+// source: (various pages) http://www.cplusplus.com/
+//
 // Citation: For help reading in an "int" from command-line arguments
 // source: https://stackoverflow.com/questions/2797813/how-to-convert-a-command-line-argument-to-int
 //
@@ -17,17 +20,10 @@
 #include <iostream>   // cerr
 #include <sstream>    // istringstream, ostringstream
 #include <fstream>    // ofstream
-#include <iomanip>    // setw(), setfill()
 #include <string>     // string, stoi()
 #include <random>     // uniform_int_distribution, random_device, mt19937
-#include <locale>     // isdigit()
 #include <error.h>    // perror()
 
-double increment_filename()
-{
-  std::ostringstream oss;
-  oss << std::setw(10) << std::setfill('0')
-}
 
 int main(int argc, char* argv[])
 {
@@ -38,6 +34,8 @@ int main(int argc, char* argv[])
   if (argc == 4)
   {
     bool errFlag = false;
+
+    /* Validate Argument-1 */
     std::string arg1 = argv[1];
     try {
       std::size_t pos;
@@ -53,6 +51,7 @@ int main(int argc, char* argv[])
       errFlag = true;
     }
 
+    /* Validate Argument-2 */
     std::string arg2 = argv[2];
     try {
       std::size_t pos;
@@ -68,6 +67,7 @@ int main(int argc, char* argv[])
       errFlag = true;
     }
 
+    /* Validate Argument-3 */
     std::string arg3 = argv[3];
     try {
       std::size_t pos;
@@ -93,38 +93,33 @@ int main(int argc, char* argv[])
     exit(1);
   }
 
-  std::ofstream of ("random_numbers.txt");
-  if (of.is_open())
+  /* Generate Unique Filename */
+  std::string fname = "uni_rnums_";
+  std::string fileExt = ".txt";
+  std::ostringstream oss;
+  oss << lowRange << '_' << highRange << '_' << listSize;
+  std::string filename(fname + oss.str());
+  filename += fileExt;
+
+  /* Construct File-Stream Object */
+  std::ofstream ofs(filename);
+  if (!ofs) {
+    ofs.close();
+    std::cerr<< "Error: Opening output file.\n";
+    exit(1);
+  }
+  else
   {
-    /*
-       std::locale loc;
-       int listSize = 0;
-       if (isdigit(argv[1],loc)) listSize = atol(argv[1]);
-       else { perror("listSize"); exit(1); }
-
-       int lowRange = 0;
-       if (isdigit(argv[2],loc)) lowRange = atol(argv[2]);
-       else { perror("lowRange"); exit(1); }
-
-       int highRange = 0;
-       if (isdigit(argv[3],loc)) highRange = atol(argv[3]);
-       else { perror("highRange"); exit(1); }
-       */
-
+    /* Generate Random Numbers */
     std::random_device rd;
     std::mt19937 engine(rd());  /* generator engine with rd as seed */
     std::uniform_int_distribution<int> dist(lowRange,highRange);
 
     for (int n=0; n<listSize; ++n) {
-      of << dist(engine) << '\n';
+      ofs << dist(engine) << '\n';
     }
 
-    of.close();
-  }
-  else
-  {
-    perror("Opening random_numbers.txt");
-    exit(1);
+    ofs.close();
   }
 
   return 0;
